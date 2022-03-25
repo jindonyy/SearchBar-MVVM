151ddcc17b7bb82c765cd11fbafc9102b3583aeb
jindonyy <rsefa94@gmail.com>
2022-03-25 10:54:08 +0900

    Rename: util.js -> fetch.js 로 변경

--- static/src/utils/fetch.js
+++ static/src/utils/fetch.js
@@ -1,5 +1,5 @@
-export const fetchData = async url => {
-  const FULL_URL = 'http://localhost:3000/' + url;
+export const fetchData = async path => {
+  const FULL_URL = 'http://localhost:3000/' + path;
   const response = await fetch(FULL_URL);
   if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
   return response.json();
